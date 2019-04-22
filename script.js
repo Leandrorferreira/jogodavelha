722e5b66aee05895470409af3e7acbe1fc0f2443
Leandrorferreira <leandrorferreira95@gmail.com>
2019-04-22 11:46:35 -0300

    Adicionar função javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+let div = document.querySelectorAll('.individual');
+
+function adicionarImg(){
+   this.style.backgroundImage = 'url(imagens/juju.png)' ;
+   this.style.backgroundSize = 'cover'
+}
+
+div[0].onclick = adicionarImg;
+div[1].onclick = adicionarImg;
+div[2].onclick = adicionarImg;
+div[3].onclick = adicionarImg;
+div[4].onclick = adicionarImg;
+div[5].onclick = adicionarImg;
+div[6].onclick = adicionarImg;
+div[7].onclick = adicionarImg;
+div[8].onclick = adicionarImg;
